76693d588dc12197dcc790582557f28370bbe24b
Jhoiner <jhooiineer@gmail.com>
2022-10-06 20:02:46 -0500

    Se crea la vista para responder preguntas

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/TestBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/TestBean.java
@@ -20,6 +20,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Scope("session")
@@ -30,12 +32,15 @@ public class TestBean implements Serializable {
     @Setter
     private Test test;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private List<DetalleTest> detalleTestList;
-    @Getter @Setter
+    @Getter
+    @Setter
     public boolean testenproceso;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     public String codigo;
 
     @Autowired
@@ -44,18 +49,33 @@ public class TestBean implements Serializable {
     @Autowired
     private DetalleTestServicio detalleTestServicio;
 
+    @Autowired
+    private PreguntaServicio preguntaServicio;
+
+    @Getter
+    @Setter
+    private String respSeleccionada;
+
+    @Getter
+    @Setter
+    private int indiceDetalleTestActual;
+
+    @Getter
+    @Setter
+    private ArrayList<String> respuestas;
 
     @PostConstruct
-    public void inicializar(){
+    public void inicializar() {
         testenproceso = false;
         codigo = "";
+        respSeleccionada = "";
+        indiceDetalleTestActual = 0;
+        //respuestas = new ArrayList();
 
     }
 
-    public String validarCodigo (Usuario usuario)
-    {
-        if (codigo == "")
-        {
+    public String validarCodigo(Usuario usuario) {
+        if (codigo == "") {
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", "Ingrese un código");
             FacesContext.getCurrentInstance().addMessage("codigo_test", fm);
             return "";
@@ -63,17 +83,15 @@ public class TestBean implements Serializable {
         try {
             boolean valido = testServicio.validarCodigo(this.codigo);
 
-            if (valido)
-            {
-                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Message", "El test iniciará");
-                PrimeFaces.current().dialog().showMessageDynamic(message);
+            if (valido) {
                 iniciarTest(usuario);
+                return "responderTest.xhtml?faces-redirect=true";
             } else {
                 FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Message", "Código inválido");
                 PrimeFaces.current().dialog().showMessageDynamic(message);
             }
             return "";
-        }catch (Exception e) {
+        } catch (Exception e) {
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
             FacesContext.getCurrentInstance().addMessage("codigo_test", fm);
 
@@ -81,43 +99,70 @@ public class TestBean implements Serializable {
         return "";
     }
 
-    public void iniciarTest(Usuario usuario) throws Exception
-    {
-        test = testServicio.iniciarTest(codigo, usuario) ;
+    public void iniciarTest(Usuario usuario) throws Exception {
+        test = testServicio.iniciarTest(codigo, usuario);
         detalleTestList = detalleTestServicio.obtenerDetallesTest(test.getId());
+        obtenerRespuestas();
         this.testenproceso = true;
     }
 
-    public void marcarRespuesta(int idDetalle, String respuesta) throws Exception
-    {
-        for (int i = 0; i < detalleTestList.size(); i++)
-        {
-            DetalleTest detalleTest = detalleTestList.get(i);
-            if (detalleTest.getId() == idDetalle)
-            {
-                detalleTest.setRespuesta(respuesta);
+    public void obtenerRespuestas()  throws Exception{
+        respuestas = new ArrayList<>();
+        Pregunta pregunta = preguntaServicio.obtenerPregunta(detalleTestList.get(indiceDetalleTestActual).getPregunta().getId());
+        String respuesta = pregunta.getCorrecta();
+        respuestas.add(respuesta);
 
-                Pregunta pregunta = detalleTest.getPregunta();
-
-                if (pregunta.getCorrecta().equals(respuesta))
-                {
-                    detalleTest.setCalificacion(5);
-                } else {
-                    detalleTest.setCalificacion(0);
-                }
-                detalleTestServicio.guardarDetalle(detalleTest);
-            }
+        for (String res : pregunta.getIncorrecta()) {
+            respuestas.add(res);
         }
+        Collections.shuffle(respuestas); //desordena las respuestas
     }
 
-    public String cerrarTest(){
+
+    public String marcarRespuesta() throws Exception {
+
+        String respuesta = "";
+        try {
+            respuesta = respuestas.get(Integer.parseInt(respSeleccionada) - 1);
+        } catch (Exception e) {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", "Debe seleccionar una respuesta");
+            FacesContext.getCurrentInstance().addMessage("msj-bean", fm);
+        }
+
+        DetalleTest detalleTest = detalleTestList.get(indiceDetalleTestActual);
+
+        detalleTest.setRespuesta(respuesta);
+
+        Pregunta pregunta = detalleTest.getPregunta();
+
+        if (pregunta.getCorrecta().equals(respuesta)) {
+            detalleTest.setCalificacion(5);
+        } else {
+            detalleTest.setCalificacion(0);
+        }
+        detalleTestServicio.guardarDetalle(detalleTest);
+        indiceDetalleTestActual++;
+        respSeleccionada = "";
+
+        if (indiceDetalleTestActual == 6) {
+            cerrarTest();
+            return "/index.xhtml?faces-redirect=true";
+        }
+        obtenerRespuestas();
+        return "responderTest.xhtml?faces-redirect=true";
+    }
+
+    public String cerrarTest() {
         this.test = null;
         this.detalleTestList = new ArrayList<>();
         this.testenproceso = false;
         this.codigo = "";
+        this.respSeleccionada="";
+        this.indiceDetalleTestActual = 0;
 
         return "";//Falta retornar a revisión de respuestas
     }
-
-
 }
+
+
+
