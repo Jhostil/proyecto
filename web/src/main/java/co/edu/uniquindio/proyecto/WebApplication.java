4bde575a1fdb162ac32c72e02e105b0aa6fed4ce
cristian Solarte <cristian.solartej@uqvirtual.edu.co>
2022-10-18 21:08:14 -0500

    Punto de control (Memento)

--- web/src/main/java/co/edu/uniquindio/proyecto/WebApplication.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/WebApplication.java
@@ -9,6 +9,9 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,11 +25,11 @@ import java.util.concurrent.ExecutionException;
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+
         ClassLoader classLoader = WebApplication.class.getClassLoader();
 
         File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
-
+        FileInputStream serviceAccount = new FileInputStream("C:/Users/crist.LAPTOP-C0GK424K.000/Desktop/proyecto/web/target/classes/serviceAccountKey.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://quiztest-f0839-default-rtdb.firebaseio.com")
