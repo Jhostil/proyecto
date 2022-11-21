b07ed951d23f9315b52f266341827da777b0a475
cristian Solarte <cristian.solartej@uqvirtual.edu.co>
2022-11-20 21:13:34 -0500

    ultimos cambios

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/PreguntaBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/PreguntaBean.java
@@ -61,7 +61,7 @@ public class PreguntaBean implements Serializable {
     @Setter
     private String correcta;
 
-    @Value("web/target/classes/META-INF/resources/uploads")
+    @Value("web/src/main/resources/META-INF/resources/uploads")
     private String urlUpload;
 
     @Getter @Setter
